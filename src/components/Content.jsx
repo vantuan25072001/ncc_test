2514b6f3346b71aad6edef472d7976f68b68456c
vantuan25072001 <vantuan25072001@gmail.com>
2023-11-10 12:53:40 +0700

    reponsive

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,19 +1,77 @@
 function Content() {
   return (
-    <div className="contents">
-      <div className="content">
-        <h1 className="content-title">Lorem ipsum dolor sit asmet?</h1>
-        <p className="content-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique
-          consequat placerat. Vestibulum auctor pellentesque sem, eu posuere
-          erat hendrerit quis. Maecenas vel consequat turpis. Nam facilisis,
-          ligula in mattis sodales, augue justo tristique nulla, sed lacinia
-          ante eros ut mi. Morbi vitae diam augue. Aliquam vel mauris a nibh
-          auctor commodo. Praesent et nisi eu justo eleifend convallis. Quisque
-          suscipit maximus vestibulum. Phasellus congue mollis orci, sit amet
-          luctus augue tristique eu. Donec vulputate odio neque, sed semper
-          turpis pellentesque a.
-        </p>
+    <div>
+      <div className="contents">
+        <div className="content">
+          <h1 className="content-title">Lorem ipsum dolor sit asmet?</h1>
+          <p className="content-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            tristique consequat placerat. Vestibulum auctor pellentesque sem, eu
+            posuere erat hendrerit quis. Maecenas vel consequat turpis. Nam
+            facilisis, ligula in mattis sodales, augue justo tristique nulla,
+            sed lacinia ante eros ut mi. Morbi vitae diam augue. Aliquam vel
+            mauris a nibh auctor commodo. Praesent et nisi eu justo eleifend
+            convallis. Quisque suscipit maximus vestibulum. Phasellus congue
+            mollis orci, sit amet luctus augue tristique eu. Donec vulputate
+            odio neque, sed semper turpis pellentesque a.
+          </p>
+        </div>
+      </div>
+      <div className="main-menu">
+        <div className="menu-item grid-row--1">
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <img
+            className="menu-item--icon"
+            src="./css-icon1.png"
+            alt="Ảnh tượng trưng"
+          />
+          <p className="menu-item--text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </p>
+        </div>
+        <div className="menu-item grid-row--2">
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <img
+            className="menu-item--icon"
+            src="./html-icon2.png"
+            alt="Ảnh tượng trưng"
+          />
+          <p className="menu-item--text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </p>
+        </div>
+        <div className="menu-item grid-row--3">
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <img
+            className="menu-item--icon"
+            src="./url-icon3.png"
+            alt="Ảnh tượng trưng"
+          />
+          <p className="menu-item--text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non dui
+            sodales, faucibus libero ut, posuere felis. Donec imperdiet suscipit
+            accumsan. Aenean consequat condimentum velit ut tempor. Nam porta
+            massa in metus bibendum congue. Pellentesque ultrices liquam egestas
+            nunc at ullamcorper ultricies. Donec feugiat velit nulla, vel
+            sodales est ullamcorper id. Aenean consequat condimentum velit ut
+            tempor. Nam porta massa in metus bibendum congue. Pellentesque
+            ultrices vestibulum mattis.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer">
+      <p> Copyright &copy; 2021</p>
+    </div>
+  );
 }
 
 export default Footer;
